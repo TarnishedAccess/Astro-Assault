a7957340ba6b7e3f6ff2fd1dac6d84812cf9e224
TarnishedAccess <110725925+TarnishedAccess@users.noreply.github.com>
2023-09-25 17:45:31 +0100

    Made gems randomly rotate in either direction
    Absolutely pointless change but this is the last update today I promise

--- Scripts/gem_yellow_small.gd
+++ Scripts/gem_yellow_small.gd
@@ -3,12 +3,18 @@ class_name gem_yellow_small extends Area2D
 signal collected
 var value = 1
 var speed = -4
+var rotation_direction = randi()%2
+#Gem randomly rotates to the left or the right. Does nothing but looks better I think
 
 func _physics_process(delta):
 	speed += delta * 4
 	if speed > 6:
 		speed = 6
 	global_position.y += speed
+	if rotation_direction:
+		rotation += 2 * delta
+	else:
+		rotation += -2 * delta
 
 func _on_body_entered(body):
 	if body is Player:
